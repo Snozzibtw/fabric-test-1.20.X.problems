86e52722d6ddb9d7bf135f947335561fb30c8fc5
snozzi <oliverkjeldsberg@gmail.com>
2023-12-02 17:34:04 +0100

    proble,s

---
+++ src/main/java/net/snozzi/testmod/Testmod.java
@@ -0,0 +1,19 @@
+package net.snozzi.testmod.item.ModItems;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.snozzi.testmod.item.ModItemGroups;
+import net.snozzi.testmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/net/snozzi/testmod/TestmodClient.java
@@ -0,0 +1,11 @@
+package net.snozzi.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
